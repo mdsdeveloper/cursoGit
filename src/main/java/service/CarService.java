6bf189f55888905ddd756f6f54a6d34721d0525e
Matias <mdsmatias@hotmail.com>
2020-02-29 15:25:55 +0100

    Añade anotaciones lombok y ha añadido el metodo get modelo

--- src/main/java/service/CarService.java
+++ src/main/java/service/CarService.java
@@ -1,11 +1,28 @@
 package service;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+
+@AllArgsConstructor
+@Slf4j
 @Service
 public class CarService {
 
     String matricula = "";
     String color = "";
     int anio = 1986;
+    String modelo;
+
+    public String getModelo(int anio) {
+        if (anio > 1950) {
+            modelo = "1";
+        } else modelo = "2";
+
+
+        return modelo;
+    }
+
+
 }
